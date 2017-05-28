624bfde9b55bbf51ae92b51596a36e7df94059d2
Børge S. A <borge.s.andreassen@gmail.com>
2017-05-28 22:35:27 +0200

    Added more examples

--- modules/presets.py
+++ modules/presets.py
@@ -36,6 +36,31 @@ presets = [
     'reg_season': 10.0,
     'freq_days': 1,
     'periods': 365
-}]
+},
+{
+    'name' : "Sudan 2011-2015 (Parameter issue #1)",
+    'current_month': 227,
+    'acc_months': 58,
+    'selected_area' : [21.8, 39.1, 8.7, 22.3],
+    'log_of_y': False,
+    'time_window': 7,
+    'reg_changepoint': 0.05,
+    'reg_season': 10.0,
+    'freq_days': 1,
+    'periods': 365
+},
+{
+    'name' : "Sudan 2011-2015 (Parameter issue #2)",
+    'current_month': 227,
+    'acc_months': 58,
+    'selected_area' : [21.8, 39.1, 8.7, 22.3],
+    'log_of_y': False,
+    'time_window': 7,
+    'reg_changepoint': 1,
+    'reg_season': 10.0,
+    'freq_days': 1,
+    'periods': 365
+}
+]
 
 names = [preset['name'] for preset in presets]
